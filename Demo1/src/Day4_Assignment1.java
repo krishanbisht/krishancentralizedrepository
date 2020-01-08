8e83236ab6923a79b59cca2808ae20972ac870d0
Krishan <Krishan@LAPTOP-KNS8T9NF>
2020-01-08 18:27:53 +0530

    Day4_Assignment

--- Demo1/src/Day4_Assignment1.java
+++ Demo1/src/Day4_Assignment1.java
@@ -2,7 +2,7 @@
 import java.util.Scanner;
 
 public class Day4_Assignment1 {
-	private int x1;
+	/*private int x1;
 	private int x2;
 	public int sum(int x1,int x2)
 	{
@@ -36,5 +36,54 @@ public class Day4_Assignment1 {
 		Day4_Assignment1 obj1 = new Day4_Assignment1();
 		obj1.mul(obj1.x1,obj1.x2);
 	}
-
+*/
+	int a,b,c,d,e,f;
+		public Day4_Assignment1()
+		{
+			Scanner s = new Scanner(System.in);
+			System.out.println("Enter x1 value : ");
+			this.a = s.nextInt();
+			System.out.println("Enter x2 value : ");
+			this.b = s.nextInt();
+			System.out.println("Enter x3 value : ");
+			this.c = s.nextInt();
+			System.out.println("Enter x4 value : ");
+			this.d = s.nextInt();
+			System.out.println("Enter x5 value : ");
+			this.e = s.nextInt();
+			System.out.println("Enter x6 value : ");
+			this.f = s.nextInt();
+			s.close();
+		}
+		public int sum(int a, int b)
+		{
+			int sum_result;
+			sum_result = a+b;
+			return sum_result;
+		}
+		public int sub(int a, int b)
+		{
+			int sub_result;
+			sub_result = a-b;
+			return sub_result;
+		}
+		public void multiply(int a, int b)
+		{
+			int sum_result = this.sum(this.a, this.b);
+			int sub_result = this.sub(sum_result, this.c);
+			int sum_result2 = this.sum(sub_result, this.d);
+			int multi_result = sum_result2*this.e;
+			this.divide(multi_result,this.f);
+		}
+		
+		public void divide(int a ,int b)
+		{
+			int div_result = a/b;
+			System.out.println("Final result is :" +div_result);
+		}
+		public static void main(String[] args)
+		{
+			Day4_Assignment1 obj1 = new Day4_Assignment1();
+			obj1.multiply(obj1.a, obj1.b);
+		}
 }
